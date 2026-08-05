af01eed324a7ee3c769d76e8f5db9c9f55c19b64
ccgambin <ccgambin.cxs@gmail.com>
2026-08-05 17:06:55 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/* Ponto de entrada da aplicação */
-(function (w) {
-  function iniciar() {
-    var el = document.getElementById("app");
-    if (Login.logado()) Router.iniciar(el);
-    else Login.render(el, iniciar);
-  }
-  document.addEventListener("DOMContentLoaded", iniciar);
-})(window);
